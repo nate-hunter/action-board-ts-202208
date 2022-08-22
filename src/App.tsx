7d50dd341472a2d00fdbd9367fbd62a3c1e48767
Nate Hunter <nhunter@indemand.com>
2022-08-22 11:44:59 -0400

    added styled components to `App`

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import {
+  AppContainer,
+  CardContainer,
+  ColumnContainer,
+  ColumnTitle,
+} from "./styles";
+
 export const App = () => {
   return (
-    <div className="App">
-      <h1>Action Board</h1>
-      <p>A React + TypeScript Demo application.</p>
-    </div>
+    <>
+      <h1 style={{ marginLeft: "16px" }}>Action Board</h1>
+      {/* <p>A React + TypeScript Demo application.</p> */}
+      <AppContainer>
+        <ColumnContainer>
+          <ColumnTitle>Project:</ColumnTitle>
+          <CardContainer>Action 1</CardContainer>
+          <CardContainer>Action 2</CardContainer>
+          <CardContainer>Action 3</CardContainer>
+        </ColumnContainer>
+      </AppContainer>
+    </>
   );
 };
 
